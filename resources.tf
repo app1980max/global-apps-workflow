a65e3030edaa25629f8dc6c99adca29f14e1f270
app1980max <23049337+app1980max@users.noreply.github.com>
2026-03-28 02:56:00 +0300

    update

--- resources.tf
+++ resources.tf
@@ -5,12 +5,13 @@ module "weaviate" {
   depends_on = [kubernetes_namespace.n8n]
 }
 
-module "n8n" {
-  source = "./modules/n8n"
+module "flowise" {
+  source = "./modules/flowise"
   depends_on = [module.weaviate]
 }
 
-module "flowise" {
-  source = "./modules/flowise"
-  depends_on = [module.n8n]
-}
+#module "n8n" {
+#  source = "./modules/n8n"
+#  depends_on = [module.weaviate]
+#}
+
