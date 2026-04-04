bf1fc32678c876b483ac7dbd94f8761e5f6aec0f
app1980max <23049337+app1980max@users.noreply.github.com>
2026-04-04 22:04:08 +0300

    update

---
+++ modules/data-exec/main.tf
@@ -0,0 +1,20 @@
+curl -X POST http://weaviate.weaviate.svc.cluster.local:8080/v1/schema \
+  -H "Content-Type: application/json" \
+  -H "X-API-KEY: dev-key-123" \
+  -d '{
+    "class": "Products",
+    "description": "Stores product information",
+    "vectorizer": "none",
+    "properties": [
+      {
+        "name": "name",
+        "description": "Product name",
+        "dataType": ["string"]
+      },
+      {
+        "name": "price",
+        "description": "Product price",
+        "dataType": ["number"]
+      }
+    ]
+  }'
