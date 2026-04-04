f1d6b35f4c5f8d9d82def5b8d79aaf9c24c45743
app1980max <23049337+app1980max@users.noreply.github.com>
2026-04-04 22:42:28 +0300

    update

--- modules/weaviate/variable.tf
+++ modules/weaviate/variable.tf
@@ -11,3 +11,26 @@ variable "classes" {
     }))
   }))
 }
+
+default = [
+  {
+    name        = "Products"
+    description = "Stores product information"
+    vectorizer  = "text2vec-transformers"
+    properties  = [
+      { name = "name", dataType = ["text"], description = "Product name" },
+      { name = "description", dataType = ["text"], description = "Product description" },
+      { name = "price", dataType = ["number"], description = "Product price" }
+    ]
+  },
+  {
+    name        = "Article"
+    description = "Articles with author/content/title"
+    vectorizer  = "text2vec-transformers"
+    properties  = [
+      { name = "title", dataType = ["text"], description = "Article title" },
+      { name = "author", dataType = ["text"], description = "Article author" },
+      { name = "content", dataType = ["text"], description = "Article content" }
+    ]
+  }
+]
