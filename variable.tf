4498d116ae55f89977f6d80b11e01ff40421436c
app1980max <23049337+app1980max@users.noreply.github.com>
2026-04-05 01:00:27 +0300

    update

--- variable.tf
+++ variable.tf
@@ -10,6 +10,16 @@ variable "charts_path" {
   description = "The charts full path"
 }
 
+variable "namespace" {
+  type        = string
+  default     = "weaviate"
+  description = "Kubernetes namespace where Weaviate is deployed"
+}
 
+variable "api_keys" {
+  type        = list(string)
+  default     = ["dev-key-123"]
+  description = "List of API keys for Weaviate authentication"
+}
 
 
