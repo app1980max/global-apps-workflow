a5ed83d02f58abae17c65bbfa89fbc2b8db22eec
app1980max <23049337+app1980max@users.noreply.github.com>
2026-04-04 22:07:54 +0300

    update

--- modules/data-exec/main.tf
+++ modules/data-exec/main.tf
@@ -18,3 +18,18 @@ curl -X POST http://weaviate.weaviate.svc.cluster.local:8080/v1/schema \
       }
     ]
   }'
+
+
+
+curl -X POST http://weaviate.weaviate.svc.cluster.local:8080/v1/schema \
+  -H "Content-Type: application/json" \
+  -H "X-API-KEY: dev-key-123" \
+  -d '{
+    "class": "Users",
+    "description": "Stores user info",
+    "vectorizer": "none",
+    "properties": [
+      { "name": "username", "dataType": ["string"] },
+      { "name": "email", "dataType": ["string"] }
+    ]
+  }'
